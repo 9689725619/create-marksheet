0ee0a84b624384f8cd928977f2e3a70c6b8dba16
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-04-12 14:25:37 +0530

    Add files via upload

---
+++ create markseet in c.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+	int m3,pldc,defm,dc,ethics;
+	float per;
+	printf("enter m3");
+	scanf("%d",&m3);
+	printf("enter pldc");
+	scanf("%d",&pldc);
+	printf("enter dc");
+	scanf("%d",&dc);
+	printf("enter defm");
+	scanf("%d",&defm);
+	printf("enter ethics");
+	scanf("%d",&ethics);
+	
+	
+	
+	printf("----------------------------------------
+	-----------------------------------------------\n");
+	printf("colledge name :PEIT\n name:shubhangi\troll no. 21122\n enroll no\n");
+	printf("--------------------------------------------------------------------------------
+	------------------------------------------------------------------------------------\n");
+	printf("subject         | \t         total marks        |\t obtained marks\n");
+	printf("---------------------------------------------------------------------------------
+	---------------------------------------------------------------------------------------\n");
+	printf("m3           |\t100     |       \t");
+	printf("ethics       |\t 100      |         \t");
+	printf("defm          |\t 100       |         \t");
+	printf("pldc          |\t 100        |         \t");
+	printf("dc             |\t100        |           \t");
+	printf("------------------------------------------------------------------------------------
+	-------------------------------------------------------------------------------------\n");
+	printf("\t          |          total=500        \t");
+	printf("|          %d\n",total);
+	
+	per=total/5;
+	printf("percentage=%f",per);
+	return 0;
+	
+	
+}
